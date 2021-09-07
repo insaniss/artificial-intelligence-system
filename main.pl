d04f2d2d34b02a5e84fd04585f021c8db1e0d777
Bobur <bobur.xxi@gmail.com>
2021-09-07 23:15:17 +0300

    Added rules

--- main.pl
+++ main.pl
@@ -1,20 +1,3 @@
-male("Alberto Mayes").
-male("Richard Mayes").
-male("Aaron Mayes").
-male("John Mayes").
-male("Scott Mayes").
-male("Jose Hawkins").
-
-female("Doris Diaz").
-female("Helen Davis").
-female("Donna Mayes").
-female("Louise Carr").
-female("Ethel Hurt").
-female("Amber Meyes").
-female("Karen Mayes").
-female("Ruth Mayes").
-female("Emily Hawkins").
-
 spouse("Alberto Mayes", "Doris Diaz").
 spouse("Richard Mayes", "Helen Davis").
 spouse("Aaron Mayes", "Louise Carr").
@@ -39,3 +22,32 @@ parent("John Mayes", "Ruth Mayes").
 parent("Ethel Hurt", "Ruth Mayes").
 parent("Jose Hawkins", "Emily Hawkins").
 parent("Karen Mayes", "Emily Hawkins").
+
+male("Alberto Mayes").
+male("Richard Mayes").
+male("Aaron Mayes").
+male("John Mayes").
+male("Scott Mayes").
+male("Jose Hawkins").
+
+female("Doris Diaz").
+female("Helen Davis").
+female("Donna Mayes").
+female("Louise Carr").
+female("Ethel Hurt").
+female("Amber Meyes").
+female("Karen Mayes").
+female("Ruth Mayes").
+female("Emily Hawkins").
+
+son(X, Y) 				:- parent(Y, X), male(X).
+daughter(X, Y) 			:- parent(Y, X), female(X).
+
+father(X, Y) 			:- parent(X, Y), male(X).
+mother(X, Y) 			:- parent(X, Y), female(X).
+
+grandfather(X, Z)		:- father(X, Y), father(Y, Z); father(X, Y), mother(Y, Z).
+grandmother(X, Z)		:- mother(X, Y), father(Y, Z); mother(X, Y), mother(Y, Z).
+
+greatgrandfather(X, Z)	:- grandfather(X, Y), father(Y, Z); grandfather(X, Y), mother(Y, Z).
+greatgrandmother(X, Z) 	:- grandmother(X, Y), father(Y, Z); grandmother(X, Y), mother(Y, Z).
